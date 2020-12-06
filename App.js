edec43c1a23eee0904dceca7c5678556663f1e52
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-06 11:43:49 -0600

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [items, setItems] = useState([]);
+
+  const textInputHandler = (text) => {
+    setTask(text);
+  };
+
+  const addTask = () => {
+    setItems((currentItems) => [...currentItems, task]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter a task"
+          onChangeText={textInputHandler}
+          value={task}
+        />
+        <Button onPress={addTask} title="Add" />
+      </View>
+      <View>
+        {items.map((item, index) => (
+          <Text key={index}> {item} </Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingHorizontal: 30,
+  },
+  row: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderBottomColor: "black",
+    width: "80%",
+    borderBottomWidth: 1,
   },
 });
